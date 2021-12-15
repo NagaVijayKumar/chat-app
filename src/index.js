c2ea966aa18615f94e381d37639730da27b7ab88
Vijay Kumar <vijaykumarcseiiit@gmail.com>
2021-12-15 17:30:39 +0530

    some UI added

--- src/index.js
+++ src/index.js
@@ -16,29 +16,27 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', 'Welcome!')   // send message to only particular user
-
-    socket.broadcast.emit('message', 'A new user joined')  //Send message to all users except the curr user
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message, callback) => {
-        const filter = new Filter();
+        const filter = new Filter()
 
-        if(filter.isProfane(message)){
-           return callback("profintiy is not allowed!") 
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
         }
 
-        io.emit('message', message)      // Send messages to all users
+        io.emit('message', message)
         callback()
     })
 
-    //notify all users when a user leaves
-    socket.on('disconnect', ()=>{
-        io.emit('message', 'A user left!')
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 
-    socket.on('sendLocation',(coords, callback )=>{
-        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
-        callback()
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
