cf6dfaac3ff15c7e649f135e49516bdb417549fd
Vijay Kumar <vijaykumarcseiiit@gmail.com>
2021-12-15 15:49:44 +0530

    notify user when a new user joins or leaves the chat room

--- src/index.js
+++ src/index.js
@@ -15,11 +15,19 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', 'Welcome!')
+    socket.emit('message', 'Welcome!')   // send message to only particular user
+
+    socket.broadcast.emit('message', 'A new user joined')  //Send message to all users except the curr user
 
     socket.on('sendMessage', (message) => {
-        io.emit('message', message)
+        io.emit('message', message)      // Send messages to all users
     })
+
+    //notify all users when a user leaves
+    socket.on('disconnect', ()=>{
+        io.emit('message', 'A user left!')
+    })
+
 })
 
 server.listen(port, () => {
