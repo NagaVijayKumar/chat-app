411b5514487a91361e03e5947a0b6d762359800e
Vijay Kumar <vijaykumarcseiiit@gmail.com>
2021-12-15 16:37:47 +0530

    share real time location link to all connected users

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,19 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     socket.emit('sendMessage', message)
 })
+
+document.querySelector('#send-location').addEventListener('click',()=>{
+
+    if(!navigator.geolocation){
+        return alert("Your Browser not support to share Locatiion")
+    }
+    
+    navigator.geolocation.getCurrentPosition((position)=>{
+      
+        socket.emit('sendLocation',{
+            latitude : position.coords.latitude,
+            longitude : position.coords.longitude
+        })
+
+    })
+})
