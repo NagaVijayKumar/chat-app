1bf82223562e705b0a2d95947ecbb5d57a971fe2
Vijay Kumar <vijaykumarcseiiit@gmail.com>
2021-12-15 17:09:05 +0530

    add acknowledge message when user share location

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ io.on('connection', (socket) => {
 
     socket.on('sendLocation',(coords, callback )=>{
         io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 })
 
